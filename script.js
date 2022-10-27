5f2429d5077443418544bb8f592d74cc6bd2964e
Luke <lukeledvina19@gmail.com>
2022-10-27 11:23:51 -0400

    Set up basic project structure

---
+++ script.js
@@ -0,0 +1,28 @@
+let myLibrary = [
+    {
+        "title": "Hello world",
+        "author": "Me",
+        "pages": 25,
+        "read": true,
+    }
+];
+
+
+function displayBooks() {
+    for(let book in myLibrary) {
+        
+    }
+}
+
+// Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+function addBook() {
+
+}
